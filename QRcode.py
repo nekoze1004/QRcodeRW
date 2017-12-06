16258a78473812776523d2e196fa3effca6232f1
猫背 <nekozezaku@gmail.com>
2017-12-07 00:02:05 +0900

    いろいろ追加

--- QRcode.py
+++ QRcode.py
@@ -1,27 +1,76 @@
 from pyzbar.pyzbar import decode
-from PIL import Image
-import webbrowser
-import tkinter.filedialog
-import cv2
-import os
+import webbrowser, os, sys, cv2, qrcode, tkinter.filedialog
 
 
+# QRコードをGUIで選択する
 def fileSelect():
-    fType = [("*.jpg", "*.png")]
-    iDir = os.path.abspath(os.path.dirname(__file__))
-    fileName = tkinter.filedialog.askopenfile(filetypes=fType, initialdir=iDir)
+    fileType = [("*.jpg", "*.png")]
+    initialDir = os.path.abspath(os.path.dirname(__file__) + "/result")
+    fileName = tkinter.filedialog.askopenfile(filetypes=fileType, initialdir=initialDir)
     return fileName
 
 
-if __name__ == "__main__":
+# QRコードを読み込む
+def readQR():
     imgPath = fileSelect()
-    print(imgPath.name)
-    img = cv2.imread(imgPath.name)
-    data = decode(img)
-
     try:
-        if data[0][0][:4].decode("utf-8") == "http":
-            url = data[0][0].decode("utf-8")
-            webbrowser.open(url)
-    except UnicodeDecodeError:
+        img = cv2.imread(imgPath.name)
+        data = decode(img)
         print(data[0][0].decode("utf-8"))
+        try:
+            # 文字の先頭4文字がhttpならURLとして処理する
+            if data[0][0][:4].decode("utf-8") == "http":
+                url = data[0][0].decode("utf-8")
+                # webブラウザで開く
+                webbrowser.open(url)
+        except UnicodeDecodeError:  # URLでなければ、なにもしない
+            pass
+    except AttributeError:
+        # 画像が読み込めないエラーが発生したら
+        print("画像が見つかりませんでした")
+
+
+# 入力された文字に応じてQRコードを生成する
+def writeQR():
+    print("QRコードにしたい文字列を入力")
+    qrStr = input(">>> ")
+    QR = qrcode.make(qrStr)
+    # QRコードのファイル名を決める
+    fileName = URL2FileName(qrStr)
+    print(fileName)
+    QR.save("result/" + fileName + ".png")
+    QR.show()
+
+
+def URL2FileName(str):
+    result = str.replace("\\", "").replace("/", "_").replace("*", "") \
+        .replace(":", "").replace("?", "").replace("\"", "") \
+        .replace("<", "").replace(">", "").replace("|", "")
+    return result
+
+
+def isR(str):
+    if str == "r":
+        return 0
+    elif str == "R":
+        return 0
+    elif str == "w":
+        return 1
+    elif str == "W":
+        return 1
+    elif str == "e":
+        return 3
+    else:
+        return 4
+
+
+if __name__ == "__main__":
+    while True:
+        print("QRコード、読むか書くか R or W")
+        rw = input(">>> ")
+        if isR(rw) == 0:
+            readQR()
+        elif isR(rw) == 1:
+            writeQR()
+        elif isR(rw) == 3:
+            sys.exit()
